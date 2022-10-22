f853766d101041d336b3958d2677b34046fe021c
matheusapaiavarela <116452169+matheusapaiavarela@users.noreply.github.com>
2022-10-22 17:14:42 +0100

    Add files via upload

---
+++ aula06aa.py
@@ -0,0 +1,7 @@
+n1 = input ("Digite um valor!")
+n2 = input ("Digite um outro valor!")
+S = int (n1) + int (n2)
+print ("A soma vale", S)
+print ("A soma entre", n1, "e", n2,  "é".format (S))
+print (" A soma entre {0} e {1} vale {2}".format (n1, n2, S))
+ 
